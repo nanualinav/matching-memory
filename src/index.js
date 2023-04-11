77752fb02c4cc3e03d03f8340ac7bc2569146104
Alina Nanu <nanualinav@gmail.com>
2023-04-11 14:09:37 +0300

    Project setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { store } from './app/store.js'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
