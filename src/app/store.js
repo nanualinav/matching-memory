5fa59cdc31059245f342d64e8de64770842f6e69
Alina Nanu <nanualinav@gmail.com>
2023-04-11 18:48:09 +0300

    first commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,6 @@
-import { combineReducers, createStore } from 'redux';
+import { combineReducers } from 'redux';
+import { legacy_createStore as createStore } from 'redux';
+
 
 import { boardReducer } from '../features/board/boardSlice.js';
 
